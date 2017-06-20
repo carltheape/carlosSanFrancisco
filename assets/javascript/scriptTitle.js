518083890b956338089666ddb4f73248161c93ea
musicteachj <jimilittle3@hotmail.com>
2017-06-20 19:49:23 -0400

    added
    update

---
+++ assets/javascript/scriptTitle.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+
+var music = new Audio('assets/sounds/Murder.mp3')
+
+music.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+music.play();
+
+$('.sound').click(function() {
+  if (music.paused == false) {
+      music.pause();
+  } else {
+      music.play();
+  }
+});
+
+});
