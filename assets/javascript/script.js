b1c5c4b8d60e6f072d6a5a9286247c91bc1b82a1
Carltheape <tbfinkle@gmail.com>
2017-07-06 21:50:43 -0400

    working firebase roster

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1266,5 +1266,73 @@ displayCountryInfo();
 
     });
 
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAnDIrxPck-uh7JhwatLgC8HbMK5xQ2en4",
+    authDomain: "carlsanfran.firebaseapp.com",
+    databaseURL: "https://carlsanfran.firebaseio.com",
+    projectId: "carlsanfran",
+    storageBucket: "carlsanfran.appspot.com",
+    messagingSenderId: "751701180914"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  //initial values
+  var name = "";
+
+  // function displayHighScore() {
+    // debugger;
+    //Querying by orderByValue and limitToLast (10)<-- top 10 scores
+    database.ref().orderByChild("score").limitToLast(10).on("value", function(snapshot) {
+        var scoressss = [];
+      snapshot.forEach(function(data) {
+      console.log ("Congratulations! "+ data.val().name + "  You're in the top 10. You scored " + data.val().score);
+      // $(".panel-body").append(data.val().name + " " + data.val().score + "<br>");
+      scoressss.push("<tr><td>" + data.val().name + "</td><td>" + data.val().score + "</td><tr>");
+
+      });
+      $("#scoreStuff").html(scoressss.reverse())
+    });
+
+     // $("#score-table > tbody").append("<tr><td>" + data.val().name + "</td><td>" + data.val().score + "</td><tr>" );
+      // Handle the errors
+    // function(errorObject) {
+    //   console.log("Errors handled: " + errorObject.code);
+
+    //   };
+  // };
+
+  // displayHighScore();  //inital high score
+
+  // Capture Button Click
+  $("#logScore").on('click', function(e) {
+    e.preventDefault();
+
+    name = $("#name-input").val().trim();
+
+    console.log(score);
+
+    // Code for the push
+    database.ref().push({
+
+
+      name: name,
+      score: score,
+
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+    });
+    //Clears all of the text-boxes
+    $("#name-input").val(""),
+    // $("#score-input").val(""),
+    // displayHighScore();
+    //alert
+    alert ("Your name has been logged");
+
+  });
+
+
+
 
 }); //document ready brackets
